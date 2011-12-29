f99cf5ae36a743a5bd5c060f383d50c0f6fff7ae
Bruno Soares <brunomachadosoares@gmail.com>
2011-12-29 15:51:08 -0200

    First version of split pcap that works. It's really slow because there are lot of open's and close's (too much I/O) but... it works

--- split.c
+++ split.c
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <unistd.h>
 
+#define SPLITTED	"splitted"
+#define PKTHDR_LEN	16
+#define PCAP_HDR	24
+
 static int file_count = 0;
 
 struct dump_pcap_pkthdr {
@@ -23,19 +27,45 @@ struct dump_pcap_file_header {
 	unsigned int    linktype;
 };
 
+static void write_pcap_data(struct dump_pcap_pkthdr packet_header, unsigned char buff[])
+{
+	int fd = -1;
+	char path[300] = {0};
+
+	snprintf(path, 300, "%s_%d.cap", SPLITTED, file_count);
+
+	fd = open(path, O_WRONLY | O_APPEND, S_IRUSR | S_IWUSR);
+	if(fd > 0) {
+		write(fd, (const void *)&packet_header, PKTHDR_LEN);
+		write(fd, buff, packet_header.caplen);
+		close(fd);
+	}
+
+}
+
 static void write_pcap_header(struct dump_pcap_file_header pcap_header)
 {
+	int fd = -1;
+	char path[300] = {0};
+
+	snprintf(path, 300, "%s_%d.cap", SPLITTED, file_count);
+
+	fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if(fd > 0) {
+		write(fd, (const void *)&pcap_header, PCAP_HDR);
+		close(fd);
+	}
 }
 
 static void read_pcap_file(char *file, int split_size)
 {
-	int r     = 0;
-	int tot_r = 0;
+	int total = 0;
 	int fd    = -1;
-	unsigned char *buff = NULL;
-	int reach = (split_size * (1024 * 1024));
-	struct dump_pcap_pkthdr packet_header    = {0};
-	struct dump_pcap_file_header pcap_header = {0};
+	unsigned int r      = 0;
+	unsigned char buff[65535];
+	int threshold = (split_size * (1000 * 1000));
+	struct dump_pcap_pkthdr packet_header;
+	struct dump_pcap_file_header pcap_header;
 
 	fd = open(file, O_RDONLY);
 	if(fd < 0) {
@@ -43,28 +73,34 @@ static void read_pcap_file(char *file, int split_size)
 		return;
 	}
 
-	if(read(fd, &pcap_header, sizeof(pcap_header)) != sizeof(pcap_header)) {
+	if(read(fd, &pcap_header, PCAP_HDR) != PCAP_HDR) {
 		printf("Failed to read TCPDump Header from file: %s\n", file);
 		return;
 	}
 
-	write_pcap_header(pcap_header);
-
-	buff = malloc(pcap_header.snaplen);
-
 	while(1)
 	{
-		r = read(fd, &packet_header, sizeof(packet_header));
-		if(r != sizeof(packet_header))
+		if(total == 0)
+			write_pcap_header(pcap_header);
+
+		r = read(fd, &packet_header, PKTHDR_LEN);
+		if(r != PKTHDR_LEN)
 			break;
 
-		tot_r += r;
+		memset(buff, '\0', pcap_header.snaplen);
 
-		if(tot_r >= split_size) {
-			//no
+		r = read(fd, &buff, packet_header.caplen);
+		if(r != packet_header.caplen)
+			break;
+
+		write_pcap_data(packet_header, buff);
+
+		total += r;
+
+		if(total >= threshold) {
+			file_count++;
+			total = 0;
 		}
-
-		//lseek(fd, packet_header.caplen, SEEK_CUR);
 	}
 
 	close(fd);
@@ -82,7 +118,7 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	read_pcap_file(argv[1], 10);
+	read_pcap_file(argv[1], atoi(argv[2]));
 
 	return 0;
 }
